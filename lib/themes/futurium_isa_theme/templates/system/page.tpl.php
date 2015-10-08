784e6b9b3b771e33b66a2c6d61e972b37c5ad031
Horacio Lopes <HoracioLopes@gmail.com>
2015-10-08 16:59:14 +0200

    Page template

--- lib/themes/futurium_isa_theme/templates/system/page.tpl.php
+++ lib/themes/futurium_isa_theme/templates/system/page.tpl.php
@@ -113,7 +113,7 @@
   </div>
 </header>
 
-<div class="main-container container-fullwidth">
+<div class="main-container">
 
   <header role="banner" id="page-header">
     <?php if (!empty($site_slogan)): ?>
@@ -123,72 +123,65 @@
     <?php print render($page['header']); ?>
   </header> <!-- /#page-header -->
 
-  <div class="container-fullwidth">
+  <?php if (!empty($page['sidebar_first'])): ?>
+    <aside class="col-sm-3" role="complementary">
+      <?php print render($page['sidebar_first']); ?>
+    </aside>  <!-- /#sidebar-first -->
+  <?php endif; ?>
 
-      <?php if (!empty($page['sidebar_first'])): ?>
-        <aside class="col-sm-3" role="complementary">
-          <?php print render($page['sidebar_first']); ?>
-        </aside>  <!-- /#sidebar-first -->
-      <?php endif; ?>
-
-      <section<?php print $content_column_class; ?>>
-        <?php if( !empty($page['highlighted']) || !empty($breadcrumb) || !empty($page['help']) || !empty($action_links) || !empty($tabs['#primary']) || !empty($tabs['#secondary']) ): ?>
+  <section<?php print $content_column_class; ?>>
+    <?php if( !empty($page['highlighted']) || !empty($breadcrumb) || !empty($page['help']) || !empty($action_links) || !empty($tabs['#primary']) || !empty($tabs['#secondary']) ): ?>
+      <div class="row">
+        <div class="container">
           <div class="row">
-            <div class="container">
-              <div class="row">
-                <div class="region other-stuff">
-                  <?php if (!empty($page['highlighted'])): ?>
-                    <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-                  <?php endif; ?>
-                  <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-                  <?php print $messages; ?>
-                  <?php if (!empty($tabs)): ?>
-                    <?php print render($tabs); ?>
-                  <?php endif; ?>
-                  <?php if (!empty($page['help'])): ?>
-                    <?php print render($page['help']); ?>
-                  <?php endif; ?>
-                  <?php if (!empty($action_links)): ?>
-                    <ul class="action-links"><?php print render($action_links); ?></ul>
-                  <?php endif; ?>
-                </div>
-              </div>
+            <div class="region other-stuff">
+              <?php if (!empty($page['highlighted'])): ?>
+                <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+              <?php endif; ?>
+              <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+              <?php print $messages; ?>
+              <?php if (!empty($tabs)): ?>
+                <?php print render($tabs); ?>
+              <?php endif; ?>
+              <?php if (!empty($page['help'])): ?>
+                <?php print render($page['help']); ?>
+              <?php endif; ?>
+              <?php if (!empty($action_links)): ?>
+                <ul class="action-links"><?php print render($action_links); ?></ul>
+              <?php endif; ?>
             </div>
           </div>
-        <?php endif; ?>
-        <?php print render($page['content_top']); ?>
-        <?php print render($page['content_before_middle']); ?>
-        <?php print render($page['content_before']); ?>
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if (!empty($title)): ?>
+        </div>
+      </div>
+    <?php endif; ?>
+    <?php print render($page['content_top']); ?>
+    <?php print render($page['content_before_middle']); ?>
+    <?php print render($page['content_before']); ?>
+    <a id="main-content"></a>
+    <?php print render($title_prefix); ?>
+    <?php if (!empty($title)): ?>
+      <div class="row">
+        <div class="container">
           <div class="row">
-            <div class="container">
-              <div class="row">
-                <div class="region other-stuff">
-                  <h1 class="page-header"><?php print $title; ?></h1>
-                </div>
-              </div>
+            <div class="region other-stuff">
+              <h1 class="page-header"><?php print $title; ?></h1>
             </div>
           </div>
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php print render($page['content']); ?>
-        <?php print render($page['content_after']); ?>
-        <?php print render($page['content_after_middle']); ?>
-        <?php print render($page['content_bottom']); ?>
-      </section>
+        </div>
+      </div>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+    <?php print render($page['content']); ?>
+    <?php print render($page['content_after']); ?>
+    <?php print render($page['content_after_middle']); ?>
+    <?php print render($page['content_bottom']); ?>
+  </section>
 
-      <?php if (!empty($page['sidebar_second'])): ?>
-        <aside class="col-sm-3" role="complementary">
-          <?php print render($page['sidebar_second']); ?>
-        </aside>  <!-- /#sidebar-second -->
-      <?php endif; ?>
-
-      <div class="clearfix" style="clear:both"></div>
-
-
-  </div>
+  <?php if (!empty($page['sidebar_second'])): ?>
+    <aside class="col-sm-3" role="complementary">
+      <?php print render($page['sidebar_second']); ?>
+    </aside>  <!-- /#sidebar-second -->
+  <?php endif; ?>
 
   <footer class="footer container-fullwidth">
     <div class="acontainer">
