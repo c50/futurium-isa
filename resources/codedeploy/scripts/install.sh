f50da2df9cda9efdcbb67ba64de7a1affeb16740
Pieter Frenssen <pieter@frenssen.be>
2016-03-02 14:54:52 +0100

    Increase the default max allowed packet size to fix "MySQL has gone away" errors.

--- resources/codedeploy/scripts/install.sh
+++ resources/codedeploy/scripts/install.sh
@@ -6,5 +6,8 @@ do
   sleep 2
 done
 
+# Increase the default max allowed packet size to fix "MySQL has gone away" errors.
+sed -i '/^max_allowed_packet/c\max_allowed_packet = 256M' /etc/mysql/my.cnf
+
 bin/phing -propertyfile /usr/local/etc/subsite/subsite.ini install-pack >> /var/log/subsite/install.log 2>&1
 chown -R www-data:www-data /var/www/html/*
