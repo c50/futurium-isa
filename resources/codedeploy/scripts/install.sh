00910121e90bd84e54c9c3d2b67d98480f313cc5
Pieter Frenssen <pieter@frenssen.be>
2016-03-02 11:55:50 +0100

    Bash redirection statement should be at the end of the line.

--- resources/codedeploy/scripts/install.sh
+++ resources/codedeploy/scripts/install.sh
@@ -6,5 +6,5 @@ do
   sleep 2
 done
 
-bin/phing -propertyfile /usr/local/etc/subsite/subsite.ini install-pack 2>&1 >> /var/log/subsite/install.log
+bin/phing -propertyfile /usr/local/etc/subsite/subsite.ini install-pack >> /var/log/subsite/install.log 2>&1
 chown -R www-data:www-data /var/www/html/*
